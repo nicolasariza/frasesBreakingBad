0e92fd58b20a1ff4caa96026ed16be6f2801905c
nicolasariza <nicolas_ariza@outlook.com>
2020-03-29 17:23:23 -0500

    Updates

---
+++ precache-manifest.e1a624c6208679716216c69e2f83b5b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed7a4d06374a27531cf5477e64be48f",
+    "url": "/frasesBreakingBad/index.html"
+  },
+  {
+    "revision": "5c8cc9cf55bf58945e31",
+    "url": "/frasesBreakingBad/static/css/main.84f32007.chunk.css"
+  },
+  {
+    "revision": "cc1d76e11f7c17506483",
+    "url": "/frasesBreakingBad/static/js/2.1153efb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frasesBreakingBad/static/js/2.1153efb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8cc9cf55bf58945e31",
+    "url": "/frasesBreakingBad/static/js/main.e1652a7b.chunk.js"
+  },
+  {
+    "revision": "e25a6b35ed11bfb8eb85",
+    "url": "/frasesBreakingBad/static/js/runtime-main.82eac15c.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/frasesBreakingBad/static/media/logo.427a4adc.svg"
+  }
+]);
